af60847995698b2575baaa25a32060554411abd5
mtd <mtd@mtds-iMac.net.fpt>
2017-05-12 20:39:05 +0700

    fix

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
       <li><a href="#">Page 2</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span>  Đăng nhập</a></li>
     </ul>
   </div>
 </nav>
